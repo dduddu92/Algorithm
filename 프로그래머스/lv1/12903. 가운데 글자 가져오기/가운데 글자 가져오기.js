9a5e39af47bd53256a4b04edc97d903cc8bc00f9
Suyeon Park <100888031+dduddu92@users.noreply.github.com>
2023-01-03 11:15:44 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let middle = Math.floor(s.length / 2)
+    if(s.length % 2 === 0){
+        return s[middle-1] + s[middle]
+    } else{
+        return s[middle]
+    }
+}
